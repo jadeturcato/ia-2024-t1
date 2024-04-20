8ca2dfe4040935b8438422189bfcf8cda3a72dd1
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-20 12:43:11 -0300

    Defining the graph structure

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,33 @@
 """Implementação de uma estrutura de grafo."""
 
 
+class Graph:
+    def __init__(self):
+        self.NodeList: List[Node] = []
+        self.EdgeList: List[Edge] = []
+
+
+class Node:
+    def __init__(self, node: int, latitude: float, longitude: float):
+        self.Node: int = node
+        self.Latitude: float = latitude
+        self.Longitude: float = longitude
+
+
+class Edge:
+    def __init__(self, node1: int, node2: int, cost: float):
+        self.Node1: int = node1
+        self.Node2: int = node2
+        self.Cost: float = cost
+        self.IsVisited: bool = False
+
+    def set_is_visited(self):
+        self.IsVisited = True
+
+    def get_is_visited(self):
+        return self.IsVisited
+
+
 def read_graph(filename: str):
     graph: Graph = Graph()
     with open(filename, "rt") as input_file:
