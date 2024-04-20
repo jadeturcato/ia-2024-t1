dfc375a9fa4dd979c5b840dbeb434c8d1631ea93
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-20 12:38:11 -0300

    updating read_graph of graph.py file

--- src/graph.py
+++ src/graph.py
@@ -2,22 +2,15 @@
 
 
 def read_graph(filename: str):
-    """Le uma estrutura de grafo de um arquivo e retorna a estrutura."""
-    graph = None
+    graph: Graph = Graph()
     with open(filename, "rt") as input_file:
-        vertex_count = int(input_file.readline().strip())
-        for _ in range(vertex_count):
+        node_count: int = int(input_file.readline().strip())
+        for _ in range(node_count):
             index, latitude, longitude = input_file.readline().strip().split()
-            index, latitude, longitude = [
-                int(index), float(latitude), float(longitude)
-            ]
-            
-        edge_count = int(input_file.readline().strip())
+            graph.NodeList.append(Node(int(index), float(latitude), float(longitude)))
+
+        edge_count: int = int(input_file.readline().strip())
         for _ in range(edge_count):
-            from_vertex, to_vertex, cost = (
-                input_file.readline().strip().split()
-            )
-            from_vertex, to_vertex, cost = [
-                int(from_vertex), int(to_vertex), float(cost)
-            ]
+            from_vertex, to_vertex, cost = input_file.readline().strip().split()
+            graph.EdgeList.append(Edge(int(from_vertex), int(to_vertex), float(cost)))
     return graph
