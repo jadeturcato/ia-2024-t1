352eff5581c4e8523c71db9571107773c553a068
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-21 12:36:36 -0300

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,6 +1,8 @@
 """Funções auxiliares para o projeto"""
 
 import math
+from graph import Edge, Node
+from typing import List
 
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -8,8 +10,8 @@ def haversine(lat1, lon1, lat2, lon2):
     dLat = (lat2 - lat1) * math.pi / 180.0  # pylint: disable=invalid-name
     dLon = (lon2 - lon1) * math.pi / 180.0  # pylint: disable=invalid-name
     # convert to radians
-    lat1 = (lat1) * math.pi / 180.0
-    lat2 = (lat2) * math.pi / 180.0
+    lat1 = lat1 * math.pi / 180.0
+    lat2 = lat2 * math.pi / 180.0
     # apply formulae
     a = (
         pow(math.sin(dLat / 2), 2) +
@@ -24,3 +26,60 @@ def haversine(lat1, lon1, lat2, lon2):
 #
 # Não altere este comentário e adicione suas funções ao final do arquivo.
 #
+
+def find_neighbor(node_list: List[int], edge_list: List[Edge], goal: int):
+    node_to_analyzed: int = node_list[-1]
+    new_list: List[List[int]] = []
+    for edge in edge_list:
+        if not edge.get_is_visited():
+            if node_to_analyzed == edge.Node1:
+                if goal == edge.Node2:
+                    new_list.append(new_item_of_list(node_list.copy(), edge.Node2))
+                    set_is_visited(edge_list, node_to_analyzed, edge.Node2)
+                    return new_list
+                new_list.append(new_item_of_list(node_list.copy(), edge.Node2))
+                set_is_visited(edge_list, node_to_analyzed, edge.Node2)
+            elif node_to_analyzed == edge.Node2:
+                if goal == edge.Node1:
+                    new_list.append(new_item_of_list(node_list.copy(), edge.Node1))
+                    set_is_visited(edge_list, node_to_analyzed, edge.Node1)
+                    return new_list
+                new_list.append(new_item_of_list(node_list.copy(), edge.Node1))
+                set_is_visited(edge_list, node_to_analyzed, edge.Node1)
+    return new_list
+
+
+def new_item_of_list(node_list: List[int], new_node: int):
+    node_list.append(new_node)
+    return node_list
+
+
+def set_is_visited(edge_list: List[Edge], starting_node: int, ending_node: int):
+    edges_to_visit: List[Edge] = list(filter(lambda e: (starting_node == e.Node1 or starting_node == e.Node2) and
+                                                       (ending_node == e.Node1 or ending_node == e.Node2), edge_list))
+    for edge in edges_to_visit:
+        edge.set_is_visited(True)
+
+
+def get_length_path(node_list: List[Node], path: List[int]):
+    length_path: float = 0
+    for index in range(len(path) - 1):
+        latitude1, longitude1 = get_lat_long(node_list, path[index])
+        latitude2, longitude2 = get_lat_long(node_list, path[index + 1])
+        length_path += haversine(latitude1, longitude1, latitude2, longitude2)
+    return length_path
+
+
+def get_lat_long(node_list: List[Node], node: int):
+    node: Node = list(filter(lambda e: node == e.Node, node_list))[0]
+    return node.Latitude, node.Longitude
+
+
+def check_if_node_is_in_graph(node_list: List[Node], node: int) -> bool:
+    find_node = next(filter(lambda e: node == e.Node, node_list), None)
+    return find_node is None
+
+
+def reset_is_visited(edge_list: List[Edge]) -> None:
+    for edge in edge_list:
+        edge.set_is_visited(False)
