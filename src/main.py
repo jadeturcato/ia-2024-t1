23205be0bb33878c502df89256fed91553cc044b
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-21 12:37:02 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
 """Utilize este arquivo para depurar seus algoritmos."""
 
 from graph import read_graph, Graph
-from busca import dfs
+from busca import dfs, bfs
+from util import reset_is_visited
 
 
 if __name__ == "__main__":
     graph: Graph = read_graph("../mapas/mini_map.txt")
-    number_graph_nodes_analyzed, length_path, path = dfs(graph, 1, 6)
 
-    print(path)
+    # region DFS
+    number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal = dfs(graph, 0, 8)
+
+    print("Busca em profundidade (DFS)")
+    print("Quantidade de nós analizados: " + str(number_graph_nodes_analyzed))
+    print("Tamanho do caminho encontrado: " + str(length_found_path))
+    print("Caminho entre o nó inicial e o nó final: " + str(path_between_start_and_goal))
+    # endregion DFS
+
+    reset_is_visited(graph.EdgeList)
+
+    # region BFS
+    number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal = bfs(graph, 0, 8)
+
+    print("\nBusca em profundidade (BFS)")
+    print("Quantidade de nós analizados: " + str(number_graph_nodes_analyzed))
+    print("Tamanho do caminho encontrado: " + str(length_found_path))
+    print("Caminho entre o nó inicial e o nó final: " + str(path_between_start_and_goal))
+    # endregion BFS
