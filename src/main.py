dbbe0d5ff9e05b5a92ff2ed347e90139cd8fd9a1
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-23 21:54:34 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 """Utilize este arquivo para depurar seus algoritmos."""
 
-from graph import read_graph, Graph
-from busca import dfs, bfs
-from util import reset_is_visited
-
+from typing import List
+from graph import read_graph, Vertex
+from src.busca import dfs, bfs
+from src.util import reset_is_visited
 
 if __name__ == "__main__":
-    graph: Graph = read_graph("../mapas/mini_map.txt")
+    graph: List[Vertex] = read_graph("../mapas/mini_map.txt")
 
     # region DFS
-    number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal = dfs(graph, 0, 8)
+    number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal = dfs(graph, 0, 7)
 
     print("Busca em profundidade (DFS)")
     print("Quantidade de nós analizados: " + str(number_graph_nodes_analyzed))
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print("Caminho entre o nó inicial e o nó final: " + str(path_between_start_and_goal))
     # endregion DFS
 
-    reset_is_visited(graph.EdgeList)
+    reset_is_visited(graph)
 
     # region BFS
     number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal = bfs(graph, 0, 8)
