be6d55d26e562f27181733d6fb330b54c53a0bc4
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-23 21:54:54 -0300

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,7 +1,7 @@
 """Funções auxiliares para o projeto"""
 
 import math
-from graph import Edge, Node
+from graph import Vertex, AdjacentVertex, Coordinate
 from typing import List
 
 
@@ -27,19 +27,17 @@ def haversine(lat1, lon1, lat2, lon2):
 # Não altere este comentário e adicione suas funções ao final do arquivo.
 #
 
-def find_neighbor(node_list: List[int], edge_list: List[Edge], goal: int) -> List[List[int]]:
-    node_to_analyzed: int = node_list[-1]
+def find_neighbor(node_list: List[int], adjacent_vertex_list: List[AdjacentVertex], goal: int) -> List[List[int]]:
     new_list: List[List[int]] = []
-    for edge in edge_list:
-        if not edge.get_is_visited() and (node_to_analyzed == edge.Node1 or node_to_analyzed == edge.Node2):
-            node_to_append: int = edge.Node1 if node_to_analyzed == edge.Node2 else edge.Node2
-            if goal == node_to_append:
-                new_list.append(new_item_of_list(node_list.copy(), node_to_append))
-                set_is_visited(edge_list, node_to_analyzed, node_to_append)
+    for vertex in adjacent_vertex_list:
+        if not vertex.get_is_visited():
+            if goal == vertex.Vertex:
+                new_list.append(new_item_of_list(node_list.copy(), vertex.Vertex))
+                vertex.set_is_visited(True)
                 return new_list
             else:
-                new_list.append(new_item_of_list(node_list.copy(), node_to_append))
-                set_is_visited(edge_list, node_to_analyzed, node_to_append)
+                new_list.append(new_item_of_list(node_list.copy(), vertex.Vertex))
+                vertex.set_is_visited(True)
     return new_list
 
 
@@ -48,32 +46,26 @@ def new_item_of_list(node_list: List[int], new_node: int) -> List[int]:
     return node_list
 
 
-def set_is_visited(edge_list: List[Edge], starting_node: int, ending_node: int) -> None:
-    edges_to_visit: List[Edge] = list(filter(lambda e: (starting_node == e.Node1 or starting_node == e.Node2) and
-                                                       (ending_node == e.Node1 or ending_node == e.Node2), edge_list))
-    for edge in edges_to_visit:
-        edge.set_is_visited(True)
-
-
-def get_length_path(node_list: List[Node], path: List[int]) -> float:
+def get_length_path(graph: List[Vertex], path: List[int]) -> float:
     length_path: float = 0
     for index in range(len(path) - 1):
-        latitude1, longitude1 = get_lat_long(node_list, path[index])
-        latitude2, longitude2 = get_lat_long(node_list, path[index + 1])
+        latitude1, longitude1 = get_lat_long(graph, path[index])
+        latitude2, longitude2 = get_lat_long(graph, path[index + 1])
         length_path += haversine(latitude1, longitude1, latitude2, longitude2)
     return length_path
 
 
-def get_lat_long(node_list: List[Node], node: int) -> (float, float):
-    node: Node = list(filter(lambda e: node == e.Node, node_list))[0]
-    return node.Latitude, node.Longitude
+def get_lat_long(graph: List[Vertex], vertex: int) -> (float, float):
+    vertex: Vertex = next(filter(lambda e: vertex == e.Vertex, graph), None)
+    return vertex.Coordinate.Latitude, vertex.Coordinate.Longitude
 
 
-def check_if_node_is_in_graph(node_list: List[Node], node: int) -> bool:
-    find_node = next(filter(lambda e: node == e.Node, node_list), None)
-    return find_node is None
+def check_vertex_in_graph(graph: List[Vertex], vertex: int) -> bool:
+    find_vertex = next(filter(lambda e: vertex == e.Vertex, graph), None)
+    return find_vertex is None
 
 
-def reset_is_visited(edge_list: List[Edge]) -> None:
-    for edge in edge_list:
-        edge.set_is_visited(False)
+def reset_is_visited(graph: List[Vertex]) -> None:
+    for vertex in graph:
+        for adjacent_vertex in vertex.AdjacentVertexList:
+            adjacent_vertex.set_is_visited(False)
