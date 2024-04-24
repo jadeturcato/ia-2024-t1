33efdfd0896e0c688d0e4f240497a48b80568e86
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-04-23 21:56:13 -0300

    Update dfs.py

--- src/busca/dfs.py
+++ src/busca/dfs.py
@@ -1,11 +1,10 @@
 """Implementação da busca em profundidade."""
 
-from src.graph import Graph
 from src.util import *
 
 
-def dfs(graph: Graph, start: int, goal: int) -> (int, float, [int]):
-    if check_if_node_is_in_graph(graph.NodeList, start) or check_if_node_is_in_graph(graph.NodeList, goal):
+def dfs(graph: List[Vertex], start: int, goal: int) -> (int, float, [int]):
+    if check_vertex_in_graph(graph, start) or check_vertex_in_graph(graph, goal):
         raise Exception("Os valores start e/ou goal não existem no grafo")
 
     number_graph_nodes_analyzed: int = 0
@@ -15,10 +14,15 @@ def dfs(graph: Graph, start: int, goal: int) -> (int, float, [int]):
         number_graph_nodes_analyzed += 1
         if stack[-1][-1] == goal:
             path_between_start_and_goal = stack[-1]
-            length_found_path = get_length_path(graph.NodeList, path_between_start_and_goal)
+            length_found_path = get_length_path(graph, path_between_start_and_goal)
             return number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal
         else:
-            node = stack.pop()
-            stack.extend(find_neighbor(node, graph.EdgeList, goal))
+            node_list = stack.pop()
+            adjacent_vertex_list: List[AdjacentVertex] = next(filter(lambda e: e.Vertex == node_list[-1], graph),
+                                                              None).AdjacentVertexList
+            stack.extend(find_neighbor(node_list, adjacent_vertex_list, goal))
+
+    raise Exception(f"É impossível encontrar um caminho entre {start} e {goal}")
+
 
 """Busca um caminho entre start e goal usando busca em profundidade"""
