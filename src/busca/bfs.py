a63872a8083ed725a78037a0ecad1a92a3015c11
Leonardo Rossi Quines <110145290+Lr-Quines@users.noreply.github.com>
2024-05-01 15:53:34 -0300

    Update bfs.py

--- src/busca/bfs.py
+++ src/busca/bfs.py
@@ -1,6 +1,10 @@
 """Implementação da busca em largura."""
+from typing import List
 
-from src.util import *
+from util import *
+
+from graph import Vertex, AdjacentVertex
+from util import check_vertex_in_graph, get_length_path, find_neighbor
 
 
 def bfs(graph: List[Vertex], start: int, goal: int) -> (int, float, [int]):
@@ -11,17 +15,14 @@ def bfs(graph: List[Vertex], start: int, goal: int) -> (int, float, [int]):
     queue: List[List[int]] = [[start]]
 
     while queue:
-        number_graph_nodes_analyzed += 1
         if queue[-1][-1] == goal:
             path_between_start_and_goal = queue[-1]
             length_found_path = get_length_path(graph, path_between_start_and_goal)
             return number_graph_nodes_analyzed, length_found_path, path_between_start_and_goal
         else:
             node_list = queue.pop(0)
-            adjacent_vertex_list: List[AdjacentVertex] = next(filter(lambda e: e.Vertex == node_list[-1], graph),
-                                                              None).AdjacentVertexList
-            queue.extend(find_neighbor(node_list, adjacent_vertex_list, goal))
-
-    raise Exception(f"É impossível encontrar um caminho entre {start} e {goal}")
+            number_graph_nodes_analyzed += 1
+            adjacent_vertex_list: List[AdjacentVertex] = next(filter(lambda e: e.Vertex == node_list[-1], graph), None).AdjacentVertexList
+            queue.extend(find_neighbor(graph, node_list, adjacent_vertex_list, goal))
 
 """Busca um caminho entre start e goal usando busca em largura."""
